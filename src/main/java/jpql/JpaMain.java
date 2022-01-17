505dd989ced32ca673b684f33dfd29983790065e
qwe5507 <apvmffkdls@naver.com>
2022-01-18 06:36:08 +0900

    페치 조인 1 - 기본

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -14,28 +14,43 @@ public class JpaMain {
         tx.begin();
         //code
         try {
-            Team team = new Team();
-            em.persist(team) ;
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA) ;
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setTeam(team);
-            em.persist(member) ;
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB) ;
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1) ;
 
             Member member2 = new Member();
-            member2.setUsername("관리자2");
-            member2.setTeam(team);
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
             em.persist(member2) ;
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3) ;
 
             em.flush();
             em.clear();
 
-            String query = "select m from Team t join t.members m";
+            String query = "select t from Team t join fetch t.members";
 
-            List<Member> result = em.createQuery(query, Member.class)
+            List<Team> result = em.createQuery(query, Team.class)
                     .getResultList();
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
+            System.out.println("result.size() = " + result.size());
+
+            for (Team team : result) {
+                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println(" -> member = " + member);
+                }
             }
 
             tx.commit();
