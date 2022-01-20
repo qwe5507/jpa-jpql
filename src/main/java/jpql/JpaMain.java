fdb807cf55452d044bb00914136ca4cc1b361dc8
qwe5507 <apvmffkdls@naver.com>
2022-01-20 16:55:44 +0900

    벌크 연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -37,17 +37,13 @@ public class JpaMain {
             member3.setTeam(teamB);
             em.persist(member3) ;
 
-            em.flush();
+            //FLUSH 자동 호출
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
             em.clear();
 
-            List<Member> resultList = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
-
-            for (Member member : resultList) {
-                System.out.println("member = " + member);
-            }
-
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
